f39df821428fad4313ce98d7a4acac772d8d6672
sonya <1281855126@qq.com>
2017-10-23 19:20:17 +0800

    c

--- game_pintu/03deal.php
+++ game_pintu/03deal.php
@@ -4,7 +4,7 @@
 	$hang = $_GET["h"];
 	$lie = $_GET["l"];
 
-	$tmp = $hang;
+	/*$tmp = $hang;
 	if($tmp<2 && $ge[++$tmp][$lie]==8){
 		$ge[$tmp][$lie] = $ge[$hang][$lie];
 		$ge[$hang][$lie] = 8;
@@ -28,10 +28,27 @@
 	if($tmp>0 && $ge[$hang][--$tmp]==8){
 		$ge[$hang][$tmp] = $ge[$hang][$lie];
 		$ge[$hang][$lie] = 8;
+	}*/
+	if($hang<2 && $ge[$hang+1][$lie]==8){
+		$ge[$hang+1][$lie] = $ge[$hang][$lie];
+		$ge[$hang][$lie] = 8;
+	}
+	if($hang>0 && $ge[$hang-1][$lie]==8){
+		$ge[$hang-1][$lie] = $ge[$hang][$lie];
+	//	echo $ge[$tmp][$lie]."----".$ge[$hang][$lie]."<br>";
+		$ge[$hang][$lie] = 8;
 	}
 
+	if($lie<2 && $ge[$hang][$lie+1]==8){
+		$ge[$hang][$lie+1] = $ge[$hang][$lie];
+		$ge[$hang][$lie] = 8;
+	}
+	if($lie>0 && $ge[$hang][$lie-1]==8){
+		$ge[$hang][$lie-1] = $ge[$hang][$lie];
+	//	echo $ge[$tmp][$lie]."----".$ge[$hang][$lie]."<br>";
+		$ge[$hang][$lie] = 8;
+	}
 
-	
 //print_r($ge);
 	$_SESSION["ge"] = $ge;
 	header("location:02show.php");
